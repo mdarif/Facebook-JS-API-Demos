d2954dd4df50b9ab8f473f7b4ed5ddb129f2217c
Mohammed Arif <arif_iq@yahoo.co.in>
2013-12-10 14:48:48 +0530

    Adding the updated files

---
+++ js/fb.share.js
@@ -0,0 +1,149 @@
+/* Facebook implementation main scripting file *
+ * Author: SapientNitro (2011) (http://www.sapient.com)
+ * @version 1.0
+ */
+
+/* FBDemo (our namespace name) and undefined are passed here 
+ * To ensure 1. Namespace can be modified locally and isn't
+ * overwritten outside of our function context
+ * 2. The value of undefined is guaranteed as being truly
+ * Undefined. This is to avoid issues with undefined being
+ * Mutable pre-ES5.
+ */
+
+/*jshint forin:true, noarg:true, eqeqeq:true, bitwise:true, undef:true, curly:true, browser:true, devel:true, indent:4, maxerr:50, jquery:true */
+
+/*jslint devel: true, nomen: true, unparam: true, sloppy: true, indent: 4 */
+
+/*global FB:false*/
+
+(function(FBDemo, $, undefined) {
+	/**
+	 * Logging function, for debugging mode
+	 */
+	jQuery.log = function(message) {
+		if (FBDemo.config.debug && (typeof window.console !== 'undefined' && typeof window.console.log !== 'undefined') && console.debug) {
+			console.debug(message);
+		}
+		/*else {
+            alert(message);
+        }*/
+	};
+
+	FBDemo.facebook = (function() {
+		function _facebook() {
+			/*
+			 * Object of the current object
+			 */
+			var _this = this;
+
+			/**
+			 * Init call
+			 * Call various methods require by pages after load
+			 */
+			this.init = function() {
+				_this.FBInit();
+				_this.FBLogin();
+				return this;
+			};
+
+			/*
+			 * Click event for FB login if required
+			 */
+			this.FBLogin = function() {
+				$(function() {
+					$(FBDemo.config.FBLogin).click(function() {
+						FB.getLoginStatus(function(response) {
+							$.log("response.status: " + response.status);
+							if (response.status === "unknown" || response.status === "not_authorized") {
+								_this.facebookLogin();
+							} else if (response.status === "connected") {
+								_this.fbShare();
+							}
+						});
+					});
+				});
+			};
+
+			/*
+			 * Initialize Facebook
+			 */
+			this.FBInit = function() {
+				FB.init({
+					appId: FBDemo.config.appId,
+					status: true,
+					cookie: true,
+					xfbml: true
+				});
+
+				/*FB.Event.subscribe('auth.login', function() {
+					_this.fbShare();
+				});*/
+
+				/**
+				 * To determine if a user is connected to your app
+				 */
+				FB.getLoginStatus(function(response) {
+					if (response.status === 'connected') {
+						/* the user is logged in and connected to your
+						 * app, and response.authResponse supplies
+						 * the user's ID, a valid access token, a signed
+						 * request, and the time the access token
+						 * and signed request each expire
+						 */
+						var uid = response.authResponse.userID,
+							accessToken = response.authResponse.accessToken;
+						//_this.fbShare();
+					} else if (response.status === 'not_authorized') {
+						$.log("You aren't authorized");
+					}
+				});
+			};
+
+			/*
+			 * If Facebook login already then do the fb share
+			 */
+			this.facebookLogin = function() {
+				FB.login(function(response) {
+					if (response.status === "connected") {
+						$.log("User is logged in and granted some permissions.");
+						_this.fbShare();
+					} else if ((response.status === 'not_authorized') || response.authResponse === null) {
+						$.log("You aren't authorized");
+					}
+				});
+			};
+			/*
+			 * FB login callback
+			 */
+			this.fbShare = function() {
+
+				FB.ui({
+						method: 'feed',
+						name: 'Facebook Dialogs',
+						link: 'https://developers.facebook.com/docs/dialogs/',
+						picture: 'http://fbrell.com/f8.jpg',
+						caption: 'Reference Documentation',
+						description: 'Dialogs provide a simple, consistent interface for applications to interface with users.',
+						display: 'iframe'
+						//display: 'popup'
+					},
+					function(response) {
+						if (response && response.post_id) {
+							$.log('Post was published!');
+						} else {
+							$.log('Post was not published!');
+						}
+					}
+				);
+			};
+			return this.init();
+		}
+
+		return new _facebook();
+	}());
+
+	/**
+	 * Check to evaluate whether 'FBDemo' exists in the global namespace - if not, assign window.FBDemo an object literal
+	 */
+})(window.FBDemo = window.FBDemo || {}, jQuery);
